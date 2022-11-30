4a1e07339eeb950efba6f3dd9e94fdb6b203131b
«Ksenia <ksen29ph@gmail.com>
2022-11-30 14:29:57 +0300

    Add 0 task

--- Les1/Program.cs
+++ Les1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите пограмму, которая на вход принимает число и выдает его квадрат
+// Например: 4->16
+// -3->9
+
+int number = int.Parse(Console.ReadLine())
+
+int result = number*number;
+
+Console.WriteLine(result);
